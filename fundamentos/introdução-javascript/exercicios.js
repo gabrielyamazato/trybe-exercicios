2b7b381617ade9aefc566b503ab320e3fc621133
Gabriel <gabrielyamazato@gmail.com>
2022-10-20 18:15:35 -0300

    inicio dos exercicios dia 1 javascript

---
+++ fundamentos/introdução-javascript/exercicios.js
@@ -0,0 +1,15 @@
+const a = 8;
+const b = 5;
+
+let adicao = a+b;
+let subtracao = a-b;
+let multiplicacao = a*b;
+let divisao = a/b;
+let modulo = a%b;
+
+if (a > b) {
+    console.log('8 é maior')
+}
+else {
+    console.log('5 é maior')
+}
