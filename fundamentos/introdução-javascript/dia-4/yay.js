a7666a8c88dd46bea95c03e1985b6921f50cd020
Gabriel <gabrielyamazato@gmail.com>
2022-10-25 18:27:17 -0300

    conteudo do course

--- fundamentos/introdução-javascript/dia-4/yay.js
+++ fundamentos/introdução-javascript/dia-4/yay.js
@@ -32,3 +32,36 @@ let car = {
 for (let carro in car) {
     console.log(carro + ': ' + car[carro])
 }
+
+//funções
+
+let statusCarro = 'desligado';
+let aceleracao = 0;
+let rotacaoDoVolante = 0;
+
+function ligarDesligar() {
+    if (statusCarro === 'desligado'){
+        statusCarro = 'ligado';
+    }   else {
+        statusCarro = 'desligado';
+    }
+        return statusCarro; 
+}
+
+function acelerar(incremento) {
+    aceleracao = aceleracao + incremento;
+
+        return 'Acelerando a ' + aceleracao + 'm/s²'
+}
+
+function frear(decremento){
+    aceleracao = aceleracao - decremento;
+        return 'Desacelerando para ' + aceleracao + 'm/s²'
+}
+
+function girarVolante(anguloRotacao){
+    rotacaoDoVolante = anguloRotacao;
+        return rotacaoDoVolante + 'º'
+}
+
+console.log(ligarDesligar())
