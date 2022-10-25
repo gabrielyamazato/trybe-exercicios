28f78711f5b07cbe622c90b0b38a2ba881a4f840
Gabriel <gabrielyamazato@gmail.com>
2022-10-25 20:57:11 -0300

    só tristeza

---
+++ fundamentos/introdução-javascript/dia-4/exercicios2.js
@@ -0,0 +1,19 @@
+// 1
+
+// function verificaPalindrome(ver){
+//     let palavra = ver.toLowerCase().replace('');
+//     let palavraReverso = palavra.split('').reverse().join('');
+
+//     return palavraReverso === palavra;
+    
+// };
+
+// verificaPalindrome('paralelepipedo')
+
+let a = 5;
+let b = 2;
+console.log(a);
+a += b;
+console.log(a);
+b -= a;
+console.log(b);
