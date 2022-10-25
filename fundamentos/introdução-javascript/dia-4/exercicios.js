5e363bf2aad77dfd0e4b9a66281260eaa02db110
Gabriel <gabrielyamazato@gmail.com>
2022-10-25 18:54:17 -0300

    exercicios for in

--- fundamentos/introdução-javascript/dia-4/exercicios.js
+++ fundamentos/introdução-javascript/dia-4/exercicios.js
@@ -4,6 +4,27 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
   };
 
-console.log('Bem vinda, ' + info.personagem)
+for (let key in info) {
+    console.log(info[key]);
+}
+
+//exercicio 2
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: 
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    
+  };
+
+    console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos.titulo); 
+
