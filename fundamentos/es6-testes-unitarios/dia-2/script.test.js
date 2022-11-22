e8b82d4be6cb347db530f0fbfe153105ffc5e84b
Gabriel <gabrielyamazato@gmail.com>
2022-11-22 10:32:55 -0300

    exercicio 1 ok

--- fundamentos/es6-testes-unitarios/dia-2/script.test.js
+++ fundamentos/es6-testes-unitarios/dia-2/script.test.js
@@ -1,5 +1,13 @@
 const myRemove = require('./script');
 
-test('verify array', () => {
-    expect(myRemove([1, 2, 3, 4], 3)).toBe([1, 2, 3, 4], 3);
+test('verify expected array', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+});
+
+test('verify NOT expected array', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+});
+
+test('verify expected array', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
 });
